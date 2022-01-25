b5de90c60f0b949503d73b461861b6fb1d887247
FenZeRir <greywolf9645@gmail.com>
2022-01-26 00:39:13 +0300

    v2

--- project_0/game_v3.py
+++ project_0/game_v3.py
@@ -46,3 +46,4 @@ def score_game(random_predict) -> int:
     score = int(np.mean(count_ls))
     print(f'Ваш алгоритм угадывал в среднем за {score} попыток')
     return(score)
+score_game(random_predict)
